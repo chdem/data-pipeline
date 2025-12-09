d2e5537a56a3d7c0442532c5a5b659f9256b9ee8
ch <outframify@gmail.com>
2025-12-09 20:53:56 +0100

    chore: conditionnal including for postgres and minio

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+def remove(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+PROJECT_DIR = os.getcwd()
+include_minio = "{{cookiecutter.include_minio}}"
+include_postgres = "{{cookiecutter.include_postgres}}"
+
+print(include_minio)
+print(include_postgres)
+
+if include_minio != "yes":
+    remove(os.path.join(PROJECT_DIR, "minio"))
+
+if include_postgres != "yes":
+    remove(os.path.join(PROJECT_DIR, "postgres"))
